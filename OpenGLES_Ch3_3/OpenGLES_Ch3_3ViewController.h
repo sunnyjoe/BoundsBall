1651ae1f002816347e7d31ad083a4d13d8ef6fa4
sunnyjoe <jiaoqingsunny@gmail.com>
2016-01-21 17:18:51 +0800

    draw a circle

--- OpenGLES_Ch3_3/OpenGLES_Ch3_3ViewController.h
+++ OpenGLES_Ch3_3/OpenGLES_Ch3_3ViewController.h
@@ -9,25 +9,5 @@
 
 
 @interface OpenGLES_Ch3_3ViewController : GLKViewController
-{
-}
-
-@property (strong, nonatomic) GLKBaseEffect 
-   *baseEffect;
-@property (strong, nonatomic) AGLKVertexAttribArrayBuffer 
-   *vertexBuffer;
-@property (nonatomic) BOOL 
-   shouldUseLinearFilter;
-@property (nonatomic) BOOL 
-   shouldAnimate;
-@property (nonatomic) BOOL 
-   shouldRepeatTexture;
-@property (nonatomic) GLfloat 
-   sCoordinateOffset;
-
-- (IBAction)takeSCoordinateOffsetFrom:(UISlider *)sender;
-- (IBAction)takeShouldRepeatTextureFrom:(UISwitch *)sender;
-- (IBAction)takeShouldAnimateFrom:(UISwitch *)sender;
-- (IBAction)takeShouldUseLinearFilterFrom:(UISwitch *)sender;
-
+ 
 @end
