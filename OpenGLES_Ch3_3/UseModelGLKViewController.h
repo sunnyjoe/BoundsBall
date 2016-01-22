8955dcea7346a02b948fae6e49765fddc11e0694
sunnyjoe <jiaoqingsunny@gmail.com>
2016-01-22 18:34:53 +0800

    add use model

---
+++ OpenGLES_Ch3_3/UseModelGLKViewController.h
@@ -0,0 +1,13 @@
+//
+//  UseModelGLKViewController.h
+//  OpenGLES
+//
+//  Created by jiao qing on 22/1/16.
+//
+//
+
+#import <GLKit/GLKit.h>
+
+@interface UseModelGLKViewController : GLKViewController
+
+@end
---
+++ OpenGLES_Ch3_3/UseModelGLKViewController.m
@@ -0,0 +1,17 @@
+//
+//  UseModelGLKViewController.m
+//  OpenGLES
+//
+//  Created by jiao qing on 22/1/16.
+//
+//
+
+#import "UseModelGLKViewController.h"
+
+@interface UseModelGLKViewController ()
+
+@end
+
+@implementation UseModelGLKViewController
+
+@end
